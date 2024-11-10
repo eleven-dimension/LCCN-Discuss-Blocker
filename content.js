3dfafa30f4613c903faab3824f0c514fefb3df01
eleven-dimension <3096152319@qq.com>
2024-11-10 15:04:37 +0800

    Block more.

--- content.js
+++ content.js
@@ -1,8 +1,28 @@
 window.addEventListener('load', () => {
   console.log('Running...');
-  const elements = document.querySelectorAll(
-    '.css-xljsgz-Wrapper, section.mr-0, li.relative:nth-child(4) > a:nth-child(1)'
-  );
+  const selectors = [
+    // study tab
+    'li.relative:nth-child(1)',
+    // discuss tab
+    'li.relative:nth-child(4)',
+    // job tab
+    'li.relative:nth-child(5)',
+    // whole discuss page
+    '.css-xljsgz-Wrapper',
+    // recommendation in main page
+    'section.mr-0',
+    // recommendation in problemset page
+    '.-mr-2',
+    // study plan in problemset page
+    'div.mt-8:nth-child(2)',
+    // company problemset
+    '.company-container',
+    // topic and company page
+    '#lc-content',
+    // leetbook
+    '.css-1t4w191-WapperContainer'
+  ];
+  const elements = document.querySelectorAll(selectors.join(', '));
   elements.forEach(element => {
     element.style.display = 'none';
   });
